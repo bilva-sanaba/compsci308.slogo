80490583ff82777077efdc9613b0cf1e64dab452
ALEXANDER BLUMENSTOCK <ab546@duke.edu>
2017-02-27 19:54:55 -0500

    Altered GUI to add ScrollPane with past commands, added new CommandScrollPane and RetrievableStackPane classes

---
+++ src/GUI/TurtleViewManager.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import configuration.Trajectory;
+import javafx.scene.image.ImageView;
+
+public class TurtleViewManager {
+private ImageView myTurtleView;
+	public TurtleViewManager(TurtleView t){
+	myTurtleView=t.getImage();
+}
+public void moveTurtle(Trajectory T){
+	
+}
+}
